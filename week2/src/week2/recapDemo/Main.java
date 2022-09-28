e9e1ed1d5a08ab718f9c1d2fcfbe49d3233c24e8
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2022-09-28 19:30:19 +0300

    Add files via upload

---
+++ week2/src/week2/recapDemo/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		ReCapDemoVideo30 fourTransactions=new ReCapDemoVideo30();
+		int result=fourTransactions.sum(3,4);
+		System.out.println(result);
+		
+
+	}
+
+}
---
+++ week2/src/week2/recapDemo/ReCapDemoVideo30.java
@@ -0,0 +1,26 @@
+
+public class ReCapDemoVideo30 {
+	
+	//four transactions
+	public int sum(int number1,int number2) {
+		return number1+number2;
+		
+
+	}
+	public int divide (int number1,int number2) {
+		return number1/number2;
+		
+
+	}
+	public int multiple(int number1,int number2) {
+		return number1*number2;
+		
+
+	}
+	public int minus(int number1,int number2) {
+		return number1-number2;
+		
+
+	}
+
+}
