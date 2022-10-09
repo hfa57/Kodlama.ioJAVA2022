c571d70aa9a10010bf5952e1ddf41d07f4d3ce6d
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2022-10-09 18:37:10 +0300

    Add files via upload

---
+++ week3/Constructor/ProductManager.java
@@ -0,0 +1,11 @@
+package week3.getterAndSetter;
+
+public class ProductManager {
+    public void Add(Product product){
+        System.out.println("Ürün eklendi " + product.getName());
+    }
+
+    public void Add2(int id, String name, String description, double price, int stockAmount, String yeniOzellik){
+
+    }
+}
