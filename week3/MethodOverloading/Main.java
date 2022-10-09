25e28b1802726fad3ad54b81d6f3b6cb9ea95e3d
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2022-10-09 18:58:53 +0300

    Add files via upload

---
+++ week3/MethodOverloading/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        FourTransactions fourtransactions = new FourTransactions();
+        System.out.println(fourtransactions.sum(2,3));
+        fourtransactions.sum(2,3,5);
+    }
+}
