5b13f96ce87f5baf5e188fbdaf9d3f018f830edf
FIRATT AYDIN <56449995+hfa57@users.noreply.github.com>
2022-10-13 16:33:30 +0300

    Add files via upload

---
+++ week3/InterfaceWithPolymorphism/CustomerManager.java
@@ -0,0 +1,16 @@
+
+public class CustomerManager {
+	
+	private ICustomerDal customerDal;
+	
+	public CustomerManager(ICustomerDal customerDal) {
+		this.customerDal=customerDal;
+		
+	}
+	
+	public void add() {
+		//business codes
+		customerDal.Add();
+	}
+
+}
